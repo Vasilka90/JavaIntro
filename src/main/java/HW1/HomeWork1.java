13110532cf1bc30d74bc8a37c47d6c9733034859
Vasilka <vasi_kai@abv.bg>
2024-09-02 05:39:37 +0300

    Solution to HomeWork1

---
+++ src/main/java/HW1/HomeWork1.java
@@ -0,0 +1,5 @@
+package HW1;
+
+public class HomeWork1{
+//some solution...
+}
---
+++ src/main/java/first/demo/Firdt/FirstDemo.java
@@ -0,0 +1,8 @@
+package first.demo.Firdt;
+
+public class FirstDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+    }
+}
